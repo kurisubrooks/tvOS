69aa5edec740f914de0dfc5ad6829efb7c0a8783
Kurisu Brooks <kurisubrooks@gmail.com>
2016-07-31 20:11:02 +1000

    Load Overscan Options from Config, Remove Loader when Loading is Complete

---
+++ app/main.js
@@ -0,0 +1,21 @@
+window.$ = window.jQuery = require("jquery")
+
+const config = require("../config.js")
+
+// DOM Loaded
+$(document).ready(function() {
+    console.log("ready")
+
+    if (config.display.overscan) $("body").css("padding", config.display.overscan)
+})
+
+// DOM Content Loaded
+$(window).on("load", function() {
+    console.log("loaded")
+
+    setTimeout(function() {
+        $(".loading-container").fadeOut("fast", function() {
+            $(".loading-container").remove()
+        })
+    }, 1000)
+})
